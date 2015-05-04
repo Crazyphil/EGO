bd9de38426d4494b016f1872026aa993099f5dee
Philipp Kapfer <philipp.kapfer@gmx.at>
2015-05-04 20:26:32 +0200

    Remove automatic app theme changes by brightness

--- EGO/src/main/java/tk/crazysoft/ego/components/AppThemeWatcher.java
+++ EGO/src/main/java/tk/crazysoft/ego/components/AppThemeWatcher.java
@@ -4,10 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 
@@ -16,31 +12,22 @@ import com.luckycatlabs.sunrisesunset.dto.Location;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.TimeZone;
 
 import tk.crazysoft.ego.R;
 import tk.crazysoft.ego.preferences.Preferences;
 
-public class AppThemeWatcher extends BroadcastReceiver implements SensorEventListener {
+public class AppThemeWatcher extends BroadcastReceiver {
     public static final int THEME_DAY = 0, THEME_NIGHT = 1;
 
-    // Switch to night theme somewhere between a cloudy day and full moon
-    private static final float LIGHT_DARKNESS = 10.0f;
-    private static final int MOVING_AVERAGE_WINDOW_SIZE = 5;
-
     private final Context context;
-    private final SensorManager manager;
-
     private Location location;
     private int currentTheme;
-    private Queue<Float> movingAverage;
+    private boolean forcedTheme;
     private OnAppThemeChangedListener listener;
 
     public AppThemeWatcher(Context context, Bundle savedInstanceState) {
         this.context = context;
-        manager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
 
         LocationManager locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
         android.location.Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
@@ -53,40 +40,29 @@ public class AppThemeWatcher extends BroadcastReceiver implements SensorEventLis
 
         if (savedInstanceState != null) {
             currentTheme = savedInstanceState.getInt("currentTheme");
+            forcedTheme = savedInstanceState.getBoolean("forcedTheme");
         } else {
             calculateThemeByTime(true);
         }
     }
 
     public void onResume() {
-        Sensor lightSensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        if (lightSensor != null) {
-            movingAverage = new LinkedList<Float>();
-            // Since Android 2.3.3 (level 10), registerListener automatically calls the listener to get an initial value
-            manager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        } else {
-            movingAverage = null;
-            IntentFilter timeFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
-            timeFilter.addAction(Intent.ACTION_TIME_CHANGED);
-            timeFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-            context.registerReceiver(this, timeFilter);
-            calculateThemeByTime(false);
-        }
+        IntentFilter timeFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
+        timeFilter.addAction(Intent.ACTION_TIME_CHANGED);
+        timeFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        context.registerReceiver(this, timeFilter);
+        calculateThemeByTime(false);
     }
 
     public void onPause() {
         try {
-            if (movingAverage != null) {
-                manager.unregisterListener(this);
-            } else {
-                context.unregisterReceiver(this);
-            }
+          context.unregisterReceiver(this);
         } catch (IllegalArgumentException e) { }
     }
 
     public void onSaveInstanceState(Bundle outState) {
         outState.putInt("currentTheme", currentTheme);
+        outState.putBoolean("forcedTheme", forcedTheme);
     }
 
     public void setOnAppThemeChangedListener(OnAppThemeChangedListener listener) {
@@ -94,28 +70,17 @@ public class AppThemeWatcher extends BroadcastReceiver implements SensorEventLis
     }
 
     public int getCurrentAppTheme() {
-        if (currentTheme == THEME_DAY) {
+        int curTheme = forcedTheme ? THEME_NIGHT - currentTheme : currentTheme;
+        if (curTheme == THEME_DAY) {
             return R.style.AppTheme;
         }
         return R.style.AppTheme_Dark;
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() != Sensor.TYPE_LIGHT) {
-            return;
-        }
-
-        if (movingAverage.size() == MOVING_AVERAGE_WINDOW_SIZE) {
-            movingAverage.remove();
-        }
-        movingAverage.add(event.values[0]);
-        //Log.d("tk.crazysoft.ego.components.AppThemeWatcher", "Brightness changed to " + currentLux + " lux");
-        calculateThemeByBrightness();
+    public void toggleAppTheme() {
+        forcedTheme = !forcedTheme;
+        fireAppThemeChangedListener();
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -135,34 +100,12 @@ public class AppThemeWatcher extends BroadcastReceiver implements SensorEventLis
         }
 
         if (!setBaseline && theme != currentTheme) {
+            forcedTheme = false;
             currentTheme = theme;
-            //Log.d("tk.crazysoft.ego.components.AppThemeWatcher", "Setting theme to " + currentTheme + " because of time");
             fireAppThemeChangedListener();
         } else if (setBaseline) {
             currentTheme = theme;
         }
-    }
-
-    private void calculateThemeByBrightness() {
-        float currentLux = getAverageLux();
-        if (currentTheme == THEME_DAY && currentLux < LIGHT_DARKNESS) {
-            currentTheme = THEME_NIGHT;
-            //Log.d("tk.crazysoft.ego.components.AppThemeWatcher", "Setting theme to " + currentTheme + " because it's dark");
-            fireAppThemeChangedListener();
-        } else if (currentTheme == THEME_NIGHT && currentLux >= LIGHT_DARKNESS) {
-            currentTheme = THEME_DAY;
-            //Log.d("tk.crazysoft.ego.components.AppThemeWatcher", "Setting theme to " + currentTheme + " because it's light");
-            fireAppThemeChangedListener();
-        }
-    }
-
-    private float getAverageLux() {
-        float average = 0;
-        for (Float f : movingAverage) {
-            average += f;
-        }
-        //Log.d("tk.crazysoft.ego.components.AppThemeWatcher", "Current brightness average: " + (average / movingAverage.size()));
-        return average / movingAverage.size();
     }
 
     private void fireAppThemeChangedListener() {
@@ -171,7 +114,7 @@ public class AppThemeWatcher extends BroadcastReceiver implements SensorEventLis
         }
     }
 
-    public interface OnAppThemeChangedListener {
-        public void onAppThemeChanged(int newTheme);
+    interface OnAppThemeChangedListener {
+        void onAppThemeChanged(int newTheme);
     }
 }
